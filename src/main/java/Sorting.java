eb3a6e0971aa37073757da2c25d436a0b6ab3c9e
xiyehu2 <xiyehu2@illinois.edu>
2018-04-04 12:20:42 -0500

    Finished BubbleSort

--- src/main/java/Sorting.java
+++ src/main/java/Sorting.java
@@ -32,7 +32,16 @@ public class Sorting {
      */
     @SuppressWarnings("unused")
     private static int[] bubbleSort(final int[] array) {
-        return null;
+        for (int i = 1; i < array.length; i++) {
+            for (int j = 1; j < array.length; j++) {
+                if (array[j] < array[j - 1]) {
+                    int temp = array[j];
+                    array[j] = array[j - 1];
+                    array[j - 1] = temp;
+                }
+            }
+        }
+        return array;
     }
 
     /**
